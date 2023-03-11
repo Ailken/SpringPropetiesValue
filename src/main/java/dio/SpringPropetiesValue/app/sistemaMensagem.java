4dc769b8440aede8055411c9b0194545a0b85c85
Ailken <ailkencf@gmail.com>
2023-03-11 01:11:33 -0300

    configuration Properties

--- src/main/java/dio/SpringPropetiesValue/app/sistemaMensagem.java
+++ src/main/java/dio/SpringPropetiesValue/app/sistemaMensagem.java
@@ -1,5 +1,6 @@
 package dio.SpringPropetiesValue.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,19 +11,13 @@ import java.util.List;
 
 @Component
 public class sistemaMensagem implements CommandLineRunner {
-    @Value("${nome:NoReply-DIO}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones =
-            new ArrayList<>(Arrays.asList(new Long[]{81995000000L}));
-
+    @Autowired
+    private Rementente rementente;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome
-                + "\nE-mail:" + email
-                + "\nCom telefones para contato: " + telefones);
+        System.out.println("Mensagem enviada por: " + rementente.getNome()
+                + "\nE-mail:" + rementente.getEmail()
+                + "\nCom telefones para contato: " + rementente.getTelefones());
         System.out.println("Seu cadastro foi aprovado");
     }
 }
